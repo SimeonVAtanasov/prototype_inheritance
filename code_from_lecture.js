1b1c551e549de7d37b2b154f562856b0a0ea19be
unknown <Simeon Atanasov@simeon.sirma.com>
2024-11-19 18:51:11 +0200

    end of internal object props

--- code_from_lecture.js
+++ code_from_lecture.js
@@ -1,3 +1,4 @@
+"use strict";
 // internal object properties
 
 // non-enumerable
@@ -12,8 +13,39 @@
 
 // non-writable
 
-let obj = { a: 1 };
-Object.defineProperty(obj, "b", { value: 2, writable: false });
-console.log("🚀 ~ ob.b:", obj.b); // => 2
-obj.b = 10;
-console.log("🚀 ~ ob.b:", obj.b); // => 2
+// let obj = { a: 1 };
+// Object.defineProperty(obj, "b", { value: 2, writable: false });
+// console.log("🚀 ~ ob.b:", obj.b); // => 2
+// obj.b = 10;
+// console.log("🚀 ~ ob.b:", obj.b); // => 2
+
+// non-configurable
+
+// let obj = {};
+// Object.defineProperty(obj, "a", {
+//   configurable: false,
+//   writable: true,
+//   value: 12,
+// });
+// // Object.defineProperty(obj, "a", { enumerable: true }); // TypeError
+// // Object.defineProperty(obj, "a", { value: 12 }); // TypeError
+// Object.defineProperty(obj, "a", { writable: false }); // allowed
+// // Object.defineProperty(obj, "a", { configurable: true }); // TypeError
+// // Object.defineProperty(obj, "a", { writable: true }); // TypeError
+// console.log("🚀 ~ obj.a:", obj.a); // => 12
+// delete obj.a; // => nothing happens
+// console.log("🚀 ~ obj.a:", obj.a); // => 12
+
+//  freeze & seal
+
+let dog = { name: "Sharo", age: 4 };
+// Object.freeze(dog);
+// dog.age = 10; // Error in strict mode
+// dog.gender = "male"; // Error in strict mode
+// console.log(dog); // { name: 'Sharo', age: 4 }
+
+Object.seal(dog);
+dog.age = 10; // OK
+// delete dog.age; // Error in strict mode
+dog.color = "brown"; // TypeError
+console.log(dog); // { name: 'Sharo', age: 10 }
