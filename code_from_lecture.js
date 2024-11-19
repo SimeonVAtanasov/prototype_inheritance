1b1f3659989ea3086b23e6b450da44c64402aab4
unknown <Simeon Atanasov@simeon.sirma.com>
2024-11-19 18:28:52 +0200

    first example is ready

--- code_from_lecture.js
+++ code_from_lecture.js
@@ -1,1 +1,9 @@
-console.log("hello");
+// internal object properties
+let obj = { a: 1, b: 2 };
+
+// when there is no key C it's going to be created on the go
+obj.c = 3;
+Object.defineProperty(obj, "d", { value: 4, enumerable: false });
+console.log("🚀 ~ ob.d:", obj.d); // => 4
+for (let key in obj) console.log(obj[key]); //1 2 3
+console.log("🚀 ~ Object.keys(obj):", Object.keys(obj)); // => ["a", "b", "c"]
