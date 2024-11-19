e5af9c43a11841a60a05b8c550d55769065262ce
unknown <Simeon Atanasov@simeon.sirma.com>
2024-11-19 19:32:43 +0200

    middle of the lecture

--- code_from_lecture.js
+++ code_from_lecture.js
@@ -44,8 +44,57 @@ let dog = { name: "Sharo", age: 4 };
 // dog.gender = "male"; // Error in strict mode
 // console.log(dog); // { name: 'Sharo', age: 4 }
 
-Object.seal(dog);
-dog.age = 10; // OK
-// delete dog.age; // Error in strict mode
-dog.color = "brown"; // TypeError
-console.log(dog); // { name: 'Sharo', age: 10 }
+// Object.seal(dog);
+// dog.age = 10; // OK
+// // delete dog.age; // Error in strict mode
+// dog.color = "brown"; // TypeError
+// console.log(dog); // { name: 'Sharo', age: 10 }
+
+// --------------------------------------------------------
+
+// prototype & inheritance
+
+// function Person(first, last, age) {
+//   this.firstName = first;
+//   this.lastName = last;
+//   this.age = age;
+// }
+// Person.prototype.homeTown = "Varna";
+// Person.prototype.sayName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// };
+
+// const person = new Person("Gergi", "Gerginov", 33);
+// console.log("🚀 ~ person:", person);
+// console.log("🚀 ~ person:", person.homeTown);
+
+// const person2 = new Person("NeGergi", "NeGerginov", 33);
+// console.log("🚀 ~ person2:", person2);
+// console.log("🚀 ~ person2:", person2.homeTown);
+// console.log("🚀 ~ person2.sayName():", person2.sayName());
+// console.log("🚀 ~ person2:", Person.prototype);
+
+// function Rectangle(width, height) {
+//   this.width = width;
+//   this.height = height;
+// }
+// Rectangle.prototype.area = function () {
+//   return this.width * this.height;
+// };
+
+// let rect = new Rectangle(3, 5);
+// console.log("🚀 ~ rect.area():", rect.area());
+
+// class Rectangle {
+//   constructor(width, height) {
+//     this.width = width;
+//     this.height = height;
+//   }
+//   area() {
+//     return this.width * this.height;
+//   }
+// }
+
+// const rec = new Rectangle(2, 5);
+// console.log("🚀 ~ rec:", rec);
+// console.log("🚀 ~ Rectangle.prototype:", rec.area()); // => 10
