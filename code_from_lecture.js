56474de986b028385bf54ed3bae645800e61485f
unknown <Simeon Atanasov@simeon.sirma.com>
2024-11-19 18:34:07 +0200

    second example

--- code_from_lecture.js
+++ code_from_lecture.js
@@ -1,9 +1,19 @@
 // internal object properties
-let obj = { a: 1, b: 2 };
 
-// when there is no key C it's going to be created on the go
-obj.c = 3;
-Object.defineProperty(obj, "d", { value: 4, enumerable: false });
-console.log("🚀 ~ ob.d:", obj.d); // => 4
-for (let key in obj) console.log(obj[key]); //1 2 3
-console.log("🚀 ~ Object.keys(obj):", Object.keys(obj)); // => ["a", "b", "c"]
+// non-enumerable
+// let obj = { a: 1, b: 2 };
+
+// // when there is no key C it's going to be created on the go
+// obj.c = 3;
+// Object.defineProperty(obj, "d", { value: 4, enumerable: false });
+// console.log("🚀 ~ ob.d:", obj.d); // => 4
+// for (let key in obj) console.log(obj[key]); //1 2 3
+// console.log("🚀 ~ Object.keys(obj):", Object.keys(obj)); // => ["a", "b", "c"]
+
+// non-writable
+
+let obj = { a: 1 };
+Object.defineProperty(obj, "b", { value: 2, writable: false });
+console.log("🚀 ~ ob.b:", obj.b); // => 2
+obj.b = 10;
+console.log("🚀 ~ ob.b:", obj.b); // => 2
